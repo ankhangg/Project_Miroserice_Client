9674aa07c5cf85a08d7adbccc9ac484e47db035d
ankhangg <luudangankhang280699@gmail.com>
2023-05-06 15:38:59 +0700

    ADD CODE 06/05

--- src/main/java/com/ankhang/controller/Authen_CodeController.java
+++ src/main/java/com/ankhang/controller/Authen_CodeController.java
@@ -8,11 +8,13 @@ import org.springframework.web.servlet.ModelAndView;
 public class Authen_CodeController {
 	@GetMapping("/home")
 	public String admin() {
+		System.out.println("--------- CALLING /HOME ---------");
 		return "home";
 	}
 	
 	@GetMapping("/homeauthen")
 	public String admin2() {
+		System.out.println("--------- CALLING /HOMEAUTHEN ---------");
 		return "home";
 	}
 	
