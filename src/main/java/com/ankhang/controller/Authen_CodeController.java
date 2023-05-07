43dc8ea098392fe5caf3047fdea84737760cf6fd
ldankhang99 <ldankhang99@gmail.com>
2023-05-07 23:22:44 +0700

    add code authen to server 07/05

--- src/main/java/com/ankhang/controller/Authen_CodeController.java
+++ src/main/java/com/ankhang/controller/Authen_CodeController.java
@@ -1,20 +1,30 @@
 package com.ankhang.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class Authen_CodeController {
+    @Autowired
+    private Environment environment;
+    
 	@GetMapping("/home")
-	public String admin() {
-		System.out.println("--------- CALLING /HOME ---------");
+	public String home() {
+		System.out.println("--------- CALLING /HOMEAUTHEN PORT: ---------" + environment.getProperty("local.server.port"));
+//      String baseUrl = String.format("%s://%s:%d", request.getScheme(), request.getServerName(), request.getServerPort());
+//      String fullUrl = baseUrl + request.getRequestURI();
+//      System.out.println("--------- CALLING " + fullUrl + " ---------");
 		return "home";
 	}
+    
+
 	
 	@GetMapping("/homeauthen")
-	public String admin2() {
-		System.out.println("--------- CALLING /HOMEAUTHEN ---------");
+	public String homeauthen() {
+		System.out.println("--------- CALLING /HOMEAUTHEN PORT: ---------" + environment.getProperty("local.server.port"));
 		return "home";
 	}
 	
